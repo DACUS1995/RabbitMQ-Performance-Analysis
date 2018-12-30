4476f20f2f046fcc3fd619f5095af7ac41867a92
DACUS1995 <studormarian@yahoo.co.uk>
2018-12-31 01:14:13 +0200

    base setup for producer

---
+++ Components/Producer/main.js
@@ -0,0 +1,37 @@
+"use strict"
+//@ts-check
+
+class Main
+{
+    constructor()
+    {}
+
+    static async run()
+    {
+
+    }
+}
+
+process.on(
+	"unhandledRejection",
+	(reason, promise) => {
+		console.log("-> unhandledRejection");
+		console.log(`Promise: ${promise}, Reason: ${reason.stack}`);
+
+		process.exit(1);
+	}
+);
+
+process.on(
+	"uncaughtException", 
+	(error) => {
+		console.log("uncaughtException");
+		console.error(error);
+		
+		process.exit(1);
+	}
+)
+
+Main.run()
+    .catch(console.error)
+
